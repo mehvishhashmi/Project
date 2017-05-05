1031ced97de47754f50fafe18501fe8840285c11
mehvishhashmi <mh449@njit.edu>
2017-05-05 18:45:03 -0400

    Response of Adding to do item action

--- index.php
+++ index.php
@@ -16,10 +16,38 @@ if($action == NULL)
 	    $suc = isUserValid($username,$password);
 	    if($suc == true)
 	    {
-	    echo ' you are in right now!';    
+	  
+	  
+	  
+	  $result = getTodoItems($_COOKIE['my_id']);
+	  include('list.php');
+
+
 	    }
 	    else{
-	    echo "Invalid user name or password!";
+	  //  echo "Invalid user name or password!";
+	    
+	     header("Location: badInfo.php");
 	    }
+
+
+	   }else if ($action == 'registrar')
+	    {
+	     // echo " we want to create a new account";
+	     $name = filter_input(INPUT_POST, 'reg_uname');
+	     if(isset($name))
+		{
+		 $pass = filter_input(INPUT_POST, 'reg_password');
+		 $exit = createUser($name,$pass);
+		 if($exit == true)
+		 {
+		  include('user_exit.php');
+		 }else {
+		  header("Location: login.php");
+		   }
+		   }
+		  }else if ($action == 'add')
+      {
+
  }
 ?>
