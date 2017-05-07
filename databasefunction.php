a0f460026aede154b4fde917e2a4c9052a41733b
mehvishhashmi <mh449@njit.edu>
2017-05-07 19:59:46 -0400

    if user already exists

--- databasefunction.php
+++ databasefunction.php
@@ -16,3 +16,45 @@ registerUser($fname,$lname,$uname,$password,$email)
   if($count > 0) {
 return true;
 }
+
+else {
+$query = 'insert into user_info(fname,lname,uname,password,email) 
+values(:fname,:lname,:uname,:password,:email)';
+$statement = $db->prepare($query);
+$statement->bindValue(':fname',$fname);
+$statement->bindValue(':lname',$lname);
+$statement->bindValue(':uname',$uname);
+$statement->bindValue(':password',$password);
+$statement->bindValue(':email',$email);
+$statement->execute();
+$statement->closeCursor();
+return false;
+}
+}
+
+function isUserRegistered($uname,$password) {
+  global $db;
+  $query = 'select * from user_info where uname = :uname and password = :password';
+  $statement = $db->prepare($query);
+  $statement->bindValue(':uname',$uname);
+  $statement->bindValue(':password',$password);
+  $statement->execute();
+  $result = $statement->fetchAll();
+  $statement->closeCursor();
+  $count = $statement->rowCount();
+  if($count == 1) {
+  setcookie('login',$uname, time()+3600);
+  $_COOKIE['login']=$uname;
+  setcookie('userid',$result[0]['id'], time()+3600);
+  $_COOKIE['userid']=$result[0]['id'];
+  setcookie('islogged',true, time()+3600);
+  $_COOKIE['islogged']=true;
+  return true;
+  } else {
+  unset($_COOKIE['login']);
+  setcookie('login',false);								      setcookie('userid',false);
+  setcookie('islogged',false);
+  return
+  false;
+  }
+										    }
