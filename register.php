20a32315353f4fe59c032239bcff53e5eda837fd
mehvishhashmi <mh449@njit.edu>
2017-05-07 18:51:24 -0400

    modification

--- register.php
+++ register.php
@@ -2,14 +2,21 @@
 <body>
   <h1> this is a register page</h1>
     <form method = 'post' action = 'index.php'>
-    <strong> First name: </strong> <input type="text" name='fname'/> <br></br>
-    	<strong> Last name: </strong> <input type="text" name="lname"/> <br> </br>
-	<strong> Email address: </strong> <input type='text' name='email'value="" /> <br> </br>
-	<strong> Phone number: </strong> <input type='text' name='phone'value="" /> <br> </br>
+        <strong> First name: </strong> <input type="text" name='fname' placeholder="first name"/>
+	<br></br>
+    	<strong> Last name: </strong> <input type="text" name="lname" placeholder="last name"/>
+	<br> </br>
+	<strong> Email address: </strong> <input type='text' name='email'value="" placeholder =
+	"email/Username" />
+	<br> </br>
+	<strong> Phone number: </strong> <input type='int' name='phone' value=""
+	placeholder = "phone number" />
+	<br> </br>
 	<strong> Birthday: </strong> <input type='text' name='birthday'/> <br> </br>
 	<strong> Gender: </strong> <input type='text' name='gender'/> <br> </br>
-	 <strong> User Name: </strong> <input type='text' name='uname'/> <br> </br>
-	 <strong> Password: </strong> <input type='text' name='password'/> <br> </br>
+ <!--	 <strong> User Name: </strong> <input type='text' name='uname'/> <br> </br> -->
+	 <strong> Password: </strong> <input type='text' name='password' value="" placeholder =
+	 "password" /> <br> </br>
 	<input type="hidden" name="action" value="register">
 	<br>
 <input type="submit" value="Register" />
