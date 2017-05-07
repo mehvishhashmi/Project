218b87602bf16a2c89edac1d6c8c4853cf181359
mehvishhashmi <mh449@njit.edu>
2017-05-06 21:10:47 -0400

    register page

--- register.php
+++ register.php
@@ -2,18 +2,19 @@
 <body>
   <h1> this is a register page</h1>
     <form method = 'post' action = 'index.php'>
-    <strong> Username: </strong> <input type='text' name='reg_uname'
-    value="" /> </ br> </ br>
-    <strong> Password: </strong> <input type="password"
-    name="reg_password" value="" /> </ br> </ br>
-    <input type="hidden" name="action" value="registrar">
-    <br>
-    <input type="submit" value="Register" />
-    </form>
-
+    <strong> First name: </strong> <input type="text" name='reg_uname'/> <br></br>
+    	<strong> Last name: </strong> <input type="text" name="reg_password"/> <br> </br>
+	<strong> Email address: </strong> <input type='text' name='reg_uname'value="" /> <br> </br>
+	<strong> Phone number: </strong> <input type='text' name='reg_uname'value="" /> <br> </br>
+	<strong> Birthday: </strong> <input type='text' name='reg_uname'/> <br> </br>
+	<strong> Gender: </strong> <input type='text' name='reg_uname'/> <br> </br>
+	<input type="hidden" name="action" value="register">
+	<br>
+<input type="submit" value="Register" />
+</form>
 <form action="login.php" method = 'post'>
-<input type='submit' value='try to login' />
 </form>
 </body>
 </html>
+
 
