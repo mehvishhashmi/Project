a463819b762179e6d62688f7cf739a4c61396a4c
mehvishhashmi <mh449@njit.edu>
2017-05-08 01:47:37 -0400

    aa

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <html>
 <body>
 <h1>To Do Application - LOGIN</h1>
+
 <form method = "post" action="index.php">
 <strong>Username:</strong><input type="test" name="username" value=""/><br><br>
 <strong>Password:</strong><input type="password" name="password" value=""/><br><br>
